7c5a08eddfc277e36525bc09725562a2b3994218
MOHAMMADsvioan <117834752+MOHAMMADsvioan@users.noreply.github.com>
2023-01-25 13:38:30 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-const btn = document.querySelector(".btn");
+const btn1 = document.querySelector(".btn1");
 
-btn.addEventListener("click", function(){
+const btn2 = document.querySelector(".btn2");
+
+const btn3 = document.querySelector(".btn3");
+
+btn1.addEventListener("click", function(){
+
+    var audioMain = new Audio("epic.mp3");
+    audioMain.play();
+
+});
+
+btn2.addEventListener("click", function(){
+
+    var audioMain = new Audio("epic.mp3");
+    audioMain.play();
+
+});
+
+btn3.addEventListener("click", function(){
 
     var audioMain = new Audio("epic.mp3");
     audioMain.play();
