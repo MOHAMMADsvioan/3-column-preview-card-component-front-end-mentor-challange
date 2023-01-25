4fc81d2527fa580ff8606dd073b20ee3303f1dad
MOHAMMADsvioan <117834752+MOHAMMADsvioan@users.noreply.github.com>
2023-01-25 13:42:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -13,14 +13,14 @@ btn1.addEventListener("click", function(){
 
 btn2.addEventListener("click", function(){
 
-    var audioMain = new Audio("epic.mp3");
-    audioMain.play();
+    var audiosec = new Audio("epic.mp3");
+    audiosec.play();
 
 });
 
 btn3.addEventListener("click", function(){
 
-    var audioMain = new Audio("epic.mp3");
-    audioMain.play();
+    var audiothird = new Audio("epic.mp3");
+    audiothird.play();
 
 });
